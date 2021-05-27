395a6904fb9a117a7d5004c7ddff1880ddf05c2b
Shinsuke Sugaya <shinsuke@apache.org>
2021-05-28 06:52:58 +0900

    fesen 0.13

--- server/src/main/java/org/codelibs/fesen/Version.java
+++ server/src/main/java/org/codelibs/fesen/Version.java
@@ -157,7 +157,8 @@ public class Version implements Comparable<Version>, ToXContentFragment {
     public static final Version V_0_11_1 = new Version(7110499, org.apache.lucene.util.Version.LUCENE_8_7_0);
     public static final Version V_0_12_0 = new Version(7120399, org.apache.lucene.util.Version.LUCENE_8_8_0);
     public static final Version V_0_12_1 = new Version(7120499, org.apache.lucene.util.Version.LUCENE_8_8_0);
-    public static final Version CURRENT = V_0_12_1;
+    public static final Version V_0_13_0 = new Version(7130399, org.apache.lucene.util.Version.LUCENE_8_8_2);
+    public static final Version CURRENT = V_0_13_0;
 
     private static final ImmutableOpenIntMap<Version> idToVersion;
     private static final ImmutableOpenMap<String, Version> stringToVersion;
